61a18288e5b3fc465528b8af6d89f867f9e0c0fc
dpi <pro@danielph.in>
2020-05-28 18:17:38 +0800

    25

--- tests/src/Functional/ConfigIgnoreHookTest.php
+++ tests/src/Functional/ConfigIgnoreHookTest.php
@@ -42,26 +42,31 @@ class ConfigIgnoreHookTest extends ConfigIgnoreBrowserTestBase {
    * Its important import/export is called directly, if called by a browser then
    * static cache will not be present.
    *
-   * @covers \Drupal\config_ignore\EventSubscriber\ConfigIgnoreEventSubscriber::getPatterns
+   * @covers \Drupal\config_ignore\EventSubscriber\ConfigIgnoreEventSubscriber::getRules
    * @covers \Drupal\config_ignore\EventSubscriber\ConfigIgnoreEventSubscriber::invalidateTags
    */
   public function testAlterHookStaticCache() {
+    // Each of these are cloned since normally result of transformStorage
+    // statically cached by \Drupal\Core\Config\ManagedStorage::$manager.
+    $sourceOriginal = $this->container->get('config.storage.export');
+    $destinationOriginal = $this->container->get('config.storage.sync');
+
     // Never called.
     $this->assertNull($this->getAlterCallCount());
 
     // Initial call caches.
-    $this->doExport();
+    $this->copyConfig(clone $sourceOriginal, clone $destinationOriginal);
     $this->assertEquals(1, $this->getAlterCallCount());
 
     // Subsequent calls read from cache.
-    $this->doExport();
+    $this->copyConfig(clone $sourceOriginal, clone $destinationOriginal);
     $this->assertEquals(1, $this->getAlterCallCount());
 
     // Writing a new value to our config triggers cache tag invalidation.
-    // This value is inconsequential.
-    $this->config('config_ignore.settings')->set('ignored_config_entities', [''])->save();
+    // This value itself is inconsequential.
+    $this->config('config_ignore.settings')->set('ignored_config_entities', ['foo.bar'])->save();
 
-    $this->doExport();
+    $this->copyConfig(clone $sourceOriginal, clone $destinationOriginal);
     $this->assertEquals(2, $this->getAlterCallCount());
   }
 
